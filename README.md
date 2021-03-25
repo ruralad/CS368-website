3df5d327906cce70637d20cd18aa9a36092b842b
Abhinav Lakshmanan <46086523+ruralad@users.noreply.github.com>
2021-03-25 22:43:58 +0530

    Initial Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# CS368-website
- 
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/home.html");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
